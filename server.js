35008ef769e1e9309dd4594616177ad8cab4cbfe
Lorenzo Terreni <lterreni@absolute.it>
2025-07-14 16:59:51 +0200

    Initial commit API

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
